635c6a6e19f671d9498c4726e38412386dbf8e7e
MarcelPalianos <mpalia01@student.bbk.ac.uk>
2025-02-25 20:21:32 +0000

    test1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Match3App()
+        }
+    }
+}
+
+@Composable
+fun Match3App() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column {
+                Text("Game is starting...", color = Color.Black) // Debug message
+                Match3Game()
+            }
+        }
+    }
+}
+
